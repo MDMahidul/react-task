e4a2c185f91edb6555a8fd3131c13563c0651529
MDMahidul <mmbmahidul007@gmail.com>
2023-10-30 20:11:52 +0600

    added footer section

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navbar from '../Pages/Shared/Navbar';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer/>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Pages/Home/SportsProgrammes/SportsProgrammes.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Container from "../../../Components/Container/Container";
+import SectionHeader from "../../../Components/SectionHeader/SectionHeader";
+
+const SportsProgrammes = () => {
+  return (
+    <div className="dark:bg-gray-800">
+      <Container>
+        <SectionHeader heading={"Our Sports Programmes"} />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
+          quisquam earum minima alias perferendis possimus cumque
+          exercitationem. Dolores quos voluptatem quia. Nam, adipisci dolorum.
+          Tenetur, aliquid a accusantium nihil placeat possimus incidunt quis
+          excepturi ex similique atque suscipit cum commodi, ad architecto. Vel,
+          sapiente officia, temporibus aliquid omnis, libero voluptate
+          asperiores maiores harum veniam eum numquam dolorum? Eius itaque
+          assumenda dolor ipsam repellendus. Magnam quod eveniet aut expedita
+          laboriosam illo suscipit quo odio fuga praesentium illum vitae
+          voluptatem, inventore a corrupti unde nisi voluptas tenetur autem enim
+          rem consequatur. Nulla, doloribus corporis! Possimus sequi, quaerat
+          ea, nemo porro cum quisquam exercitationem, itaque voluptates animi
+          iusto doloribus pariatur debitis! Reprehenderit, ut neque adipisci
+          dignissimos commodi saepe, expedita error obcaecati architecto
+          laboriosam eum explicabo quam mollitia ad animi omnis illo quidem,
+          dolorum quod perspiciatis autem repudiandae deserunt. Ea, excepturi
+          explicabo. Consequatur culpa reprehenderit a dicta eum animi molestiae
+          dignissimos fugit beatae dolorum minima, tenetur aliquid aperiam
+          tempora dolores nesciunt sequi voluptatem ea quisquam dolorem optio
+          perspiciatis ipsum. Dicta doloremque accusantium possimus nesciunt
+          repellendus nam magnam ducimus eius, rem ullam eum incidunt quia. Ad
+          necessitatibus alias soluta facilis magni nostrum, perferendis rerum
+          qui similique vero dignissimos numquam aliquid dolorum aliquam nobis
+          voluptatum, nisi, cupiditate natus ea laborum dolor perspiciatis? Id,
+          fuga molestiae laboriosam voluptatum nihil voluptatem optio cumque
+          repudiandae? Quos sequi exercitationem pariatur saepe consequuntur
+          provident? Nemo inventore quidem quae. Ullam, veniam quibusdam magni
+          odio minima animi accusantium, doloribus possimus ab sint pariatur.
+          Ipsa, expedita deleniti minus corporis, eius repellat perspiciatis cum
+          atque error quo aliquam! Amet et perspiciatis tempora rem culpa
+          praesentium aperiam quos expedita aspernatur officiis veniam,
+          veritatis obcaecati odit impedit officia perferendis reprehenderit
+          ducimus, asperiores accusamus numquam in? Mollitia aperiam enim sed
+          vitae illo quisquam nobis dolor eaque quo iusto excepturi voluptatum
+          optio iure, non ut ad odit! Assumenda voluptates laudantium possimus!
+          Perferendis suscipit natus officia nobis deleniti sequi soluta a dicta
+          debitis quae animi, omnis corporis voluptate qui voluptas magni fuga,
+          tempore distinctio esse quibusdam recusandae? Explicabo autem ducimus
+          odio quidem facere accusantium commodi sed ex delectus, nihil libero
+          deserunt, rem aliquid vel possimus minima quod corporis illo laborum!
+          Magnam modi explicabo nobis aspernatur suscipit maxime ut repellendus
+          deserunt sequi perferendis beatae necessitatibus reprehenderit facere,
+          natus expedita, officia deleniti. Delectus dolores assumenda similique
+          exercitationem excepturi quod sit perspiciatis, corrupti, mollitia
+          ipsam quam? Vitae facilis error saepe sequi placeat adipisci ipsa sed
+          velit! Vel quaerat sunt iusto, eos dicta debitis vitae assumenda
+          possimus minus in beatae reiciendis pariatur animi fugiat nobis fuga.
+          Corporis tenetur molestias possimus fuga! Laborum porro eum
+          voluptatibus vero dolore odit iure labore neque, mollitia dignissimos
+          officiis id culpa rerum suscipit sint nisi omnis nesciunt itaque
+          tempora adipisci saepe quasi asperiores minus? Error suscipit
+          inventore nostrum minus et hic odio itaque asperiores rem maiores vero
+          beatae ullam delectus voluptas ipsum accusamus fugit, tempora ipsa,
+          alias sit sequi. Illo sapiente odio harum ducimus beatae unde nisi
+          inventore deserunt doloremque saepe, quis non tempore veniam
+          reprehenderit quae recusandae, esse aliquid quaerat! Nemo quasi ipsam
+          error at alias quo consequuntur!
+        </p>
+      </Container>
+    </div>
+  );
+};
+
+export default SportsProgrammes;
